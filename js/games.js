881f46dfdfc59dadca096095e1d05e92b8e1dc02
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-07-02 23:48:27 +0200

    Start games.

---
+++ js/games.js
@@ -0,0 +1,17 @@
+var games =
+[
+ {
+  name: "34C3 Alpaka",
+  year: "2018",
+  href: "https://konradhoeffner.github.io/34c3-alpaka/",
+  source: "https://github.com/KonradHoeffner/34c3-alpaka",
+  img: "img/alpaka.jpg",
+  description: `Both players must cooperate to send all Alpakas into the spaceship above.<br>
+<br>
+Controls:<br>
+Player 1: A and D<br>
+Player 2: Left Arrow and Right Arrow<br>
+<br>
+Two player JavaScript game developed at the 34th Chaos Communication Congress in Leipzig by Simon Trümpler, Noah Rullmann and me.`
+ }
+];
