f32453e549f5a81ba311838036da644c413887f3
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-06-21 12:13:28 +0200

    add supervision

---
+++ js/supervision.js
@@ -0,0 +1,8 @@
+var supervision = [
+  {name: "Andreea Somesan", type: "master's thesis", year: 2023, title: "Optimierung der Navigation in einer Ontologie zur Beschreibung von Anwendungssystemen und Softwareprodukten im Gesundheitswesen", link: "https://hitontology.eu/public/studenttheses/Masterarbeit_Andreea_Somesan.pdf"},
+  {name: "Arne Roszeitis", type: "bachelor's thesis", year: 2022, title: "Automatische Generierung komplexer Fragen zum Informationsmanagement auf der Basis der SNIK-Ontologie", link: "https://www.snik.eu/public/bachelor-ar.pdf"},
+  {name: "Hannes Raphael Brunsch", type: "Besondere Lernleistung", year: 2022, title: "Question Answering auf SNIK", link: "https://www.snik.eu/public/bell-hrb.pdf"},
+  {name: "Max Niclas Wächtler", type: "Besondere Lernleistung", year: 2021, title: "Automatische Erstellung von Quizfragen aus einer Ontologie von Krankenhausinformationssystemen", link: "https://www.snik.eu/public/bell-mw.pdf"},
+  {name: "Thomas Pause", type: "bachelor's thesis", year: 2020, title: "Organisation von Teilgraphen semantischer Netze zur ontologiegestützten Wissensvermittlung", link: "https://www.snik.eu/public/bachelor-tp.pdf"},
+  //{name: "Stefan Faulhaber", type: "seminar thesis", year: 2017, title: "Bewertung der Datenqualität der SNIK-Ontologie (Seminararbeit)", link: "https://www.snik.eu/public/Faulhaber_Ausarbeitung74103.pdf"},
+];
